02050887a727f34cbf0c31696be105f030f8eca3
je3151 <168595398+je3151@users.noreply.github.com>
2024-04-30 14:19:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// coded by @lasjorg
-// eslint-disable-next-line no-unused-vars
-const projectName = 'portfolio';
+const header = document.querySelector(".navbar")
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
